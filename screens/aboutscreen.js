a6a20173e287cd3b0c1db7e27a505d6873ad87d4
Jiaping Liu <lemonmos@gmail.com>
2024-11-30 11:24:09 -0700

    submit lab5, get and display current date

--- screens/aboutscreen.js
+++ screens/aboutscreen.js
@@ -5,11 +5,18 @@ import { useNavigation } from "@react-navigation/native";
 
 const AboutScreen = () => {
   const navigation = useNavigation();
+  //get current date
+  const date = new Date();
+  //format date to YYYY-MM-DD
+  const formattedDate = `${date.getFullYear()}-${
+    date.getMonth() + 1
+  }-${date.getDate()}`;
   return (
     <MainLayout>
       <Text>About</Text>
       <Text>Project Name: Todo List</Text>
       <Text>Author Name: Jiaping Liu</Text>
+      <Text>Current Date: {formattedDate}</Text>
       <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
     </MainLayout>
   );
