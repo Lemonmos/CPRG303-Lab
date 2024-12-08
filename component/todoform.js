6831eb89cbe60ff9de97e476ae64c851a1c7bdad
Jiaping Liu <lemonmos@gmail.com>
2024-12-07 20:52:37 -0700

    submit lab6

--- component/todoform.js
+++ component/todoform.js
@@ -1,10 +1,26 @@
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 import React from "react";
 import { useState } from "react";
+import { useEffect } from "react";
 
 const TodoForm = ({ addTask }) => {
   const [taskText, setTaskText] = React.useState("");
+  const [taskList, setTaskList] = React.useState([]);
 
+  const handleAddTask = () => {
+    const randomIndex = Math.floor(Math.random() * taskList.tasks.length);
+    const randomTask = taskList.tasks[randomIndex];
+    setTaskText(randomTask);
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("../src/data/tasks.json");
+      const data = await response.json();
+      setTaskList(data);
+    }
+    fetchData();
+  }, []);
   return (
     <View style={styles.form}>
       <TextInput
@@ -13,13 +29,24 @@ const TodoForm = ({ addTask }) => {
         onChangeText={(text) => setTaskText(text)}
         value={taskText}
       />
-      <Button
-        title="Add"
-        onPress={() => {
-          addTask(taskText);
-          setTaskText("");
-        }}
-      />
+      <View style={styles.button}>
+        <Button
+          title="Generate Random Task"
+          onPress={() => {
+            handleAddTask();
+          }}
+        />
+      </View>
+      <View style={styles.button}>
+        <Button
+          style={styles.button}
+          title="Add"
+          onPress={() => {
+            addTask(taskText);
+            setTaskText("");
+          }}
+        />
+      </View>
     </View>
   );
 };
@@ -38,6 +65,10 @@ const styles = StyleSheet.create({
     borderColor: "#ccc",
     paddingHorizontal: 10,
     paddingVertical: 5,
+    marginRight: 10,
+  },
+  button: {
+    flex: 1,
     marginRight: 10,
   },
 });
